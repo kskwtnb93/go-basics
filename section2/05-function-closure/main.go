4928290449b3b49c564986ae5df9faa3bfd143ab
keisuke watanabe <kskwtnb93@gmail.com>
2024-12-26 22:58:21 +0900

    section2: function & closure

---
+++ section2/05-function-closure/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func funcDefer() {
+	defer fmt.Println("main func final-finish")
+	defer fmt.Println("main func semi-finish")
+	fmt.Println("hello world")
+}
+
+func trimExtension(files ...string) []string {
+	out := make([]string, 0, len(files))
+	for _, f := range files {
+		out = append(out, strings.TrimSuffix(f, ".csv"))
+	}
+	return out
+}
+
+// ()で囲うのは、複数の返り値があるとき。
+func fileChecker(name string) (string, error) {
+	f, err := os.Open(name)
+	if err != nil {
+		return "", errors.New("file not found")
+	}
+	defer f.Close()
+	return name, nil
+}
+
+func addExtention(f func(file string) string, name string) {
+	fmt.Println(f(name))
+}
+
+func multiply() func(int) int {
+	return func(n int) int {
+		return n * 1000
+	}
+}
+
+func countUp() func(int) int {
+	count := 0
+	return func(n int) int {
+		count += n
+		return count
+	}
+}
+
+func main() {
+	// defer
+	funcDefer()
+
+	files := []string{"file1.csv", "file2.csv", "file3.csv"}
+	fmt.Println(trimExtension(files...))
+
+	name, err := fileChecker("file.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(name)
+
+	// 無名関数
+	i := 1
+	func(i int) {
+		fmt.Println(i)
+	}(i)
+	f1 := func(i int) int {
+		return i + 1
+	}
+	fmt.Println(f1(i))
+
+	f2 := func(file string) string {
+		return file + ".csv"
+	}
+	addExtention(f2, "file1")
+
+	f3 := multiply()
+	fmt.Println(f3(2))
+
+	f4 := countUp()
+	for i := 1; i <= 5; i++ {
+		v := f4(2)
+		fmt.Printf("%v\n", v)
+	}
+
+}
